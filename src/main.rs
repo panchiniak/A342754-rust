6f8eff7c8912fbae4a3fea0ce03f16b44c15c1b5
Rodrigo Panchiniak Fernandes <panchiniakfernandesrodrigo@gmail.com>
2024-05-15 03:40:47 +0200

    Added array for collecting results.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ extern crate num;
 
 use num_bigint::{BigUint};
 use num_traits::FromPrimitive;
+use crate::num::ToPrimitive;
 
 fn main() {
     println!("Sequence A342754!");
 
-    const MAXSIZE: u64 = 1001;
+    const MAXSIZE: u64 = 500;
     let mut n: BigUint = BigUint::from_u64(0).unwrap();
+    let mut a: [usize; 2500] = [0; 2500];
 
     for m in 1..MAXSIZE {
         let mut k = BigUint::from_u64(2).unwrap();
@@ -17,15 +19,18 @@ fn main() {
             let modulo: BigUint = &m_big_int % &k;
             if modulo == BigUint::from_u64(0).unwrap() {
                 let mut xmod: BigUint = BigUint::from_u64(1).unwrap();
-                let mut x: u32 = 1;
+                let mut x: usize = 1;
                 while xmod != BigUint::from_u64(0).unwrap() {
-                    let pow: BigUint = BigUint::pow(&k, x);
+                    let pow: BigUint = BigUint::pow(&k, x.try_into().unwrap());
                     let diff_first: BigUint = pow - BigUint::from_u64(1).unwrap();
                     let diff_second: BigUint = m_big_int.clone() - BigUint::from_u64(1).unwrap();
                     xmod = &diff_first % &diff_second;
                     if xmod == BigUint::from_u64(0).unwrap() && x != 1 {
                         // println!("row: {m_value} - column: {k_value}", m_value=m, k_value=k);
-                        println!("{}", x);
+                        // println!("{}", x);
+                        let n_usize: usize = n.to_usize().unwrap();
+                        a[n_usize] = x;
+                        // println!("a[{number}]={value}", number=n_usize, value=a[n_usize]);
                         n += BigUint::from_u64(1).unwrap();
                     }
                     x += 1;
@@ -34,4 +39,6 @@ fn main() {
             k += BigUint::from_u64(1).unwrap();
         }
     }
+
+    println!("{:?}", a);
 }
